376daae1da20728e1c97dd090e2d6bcd68f1e36f
rafin007 <rafin.ryan.07@outlook.com>
2021-01-13 19:33:42 +0600

    Ready for deployment - 2

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  //   });
 }
 
 //conncect to database
