65f2157afdf7f885e2cdfb67829d9d21773b75e2
rafin007 <rafin.ryan.07@outlook.com>
2021-01-13 20:14:39 +0600

    build script removed

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const connectDB = require("./DB/db");
-const express = require("express");
 const expressConfig = require("./expressConfig");
-const path = require("path");
 
 //socket.io
 const app = expressConfig.init();
-
-//server static assets in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-
-  //   app.get("*", (req, res) => {
-  //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  //   });
-}
 
 //conncect to database
 connectDB();
