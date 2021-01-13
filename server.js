adb0300bb6c623de4ad984571ab32ae4ba0b0b97
rafin007 <rafin.ryan.07@outlook.com>
2021-01-13 20:46:58 +0600

    more changes

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const connectDB = require("./DB/db");
-const expressConfig = require("./expressConfig");
+const express = require("express");
+// import all the route modules
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+const profile = require("./routes/profile");
+const auth = require("./routes/auth");
 
-//socket.io
-const app = expressConfig.init();
+//intialize app
+const app = express();
+
+//init middlewares
+app.use(express.json({ extended: false }));
 
 //conncect to database
 connectDB();
+
+//define routes
+app.use("/users", users);
+app.use("/posts", posts);
+app.use("/profile", profile);
+app.use("/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
