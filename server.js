4865fbd8eaf5c1f5542cb82631505a5121a6669c
rafin007 <rafin.ryan.07@outlook.com>
2021-01-13 21:57:54 +0600

    Ready for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const connectDB = require("./DB/db");
 const express = require("express");
+const path = require("path");
 // import all the route modules
 const users = require("./routes/users");
 const posts = require("./routes/posts");
@@ -20,6 +21,14 @@ app.use("/users", users);
 app.use("/posts", posts);
 app.use("/profile", profile);
 app.use("/auth", auth);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
